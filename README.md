2a06f1855b5ad5f6282b19b4d6d65121d57b9ea9
Jonas Leonhard <jonasleonhardfl@gmail.com>
2022-08-08 17:51:37 +0200

    feat(config.lua): Added base configuration
    - This commit adds the basic config.lua file, extending and customizing
      the basic lunarvim config

---
+++ README.md
@@ -0,0 +1,83 @@
+<img src="https://shields.io/badge/jonasleonhard.de-offline-red?style=flat-square&logo=statuspal" />
+
+<img src="https://shields.io/badge/pipelines-offline-red?style=flat-square&logo=github" />
+
+<br/><br/>
+
+# Description
+
+Custom <a href="https://www.lunarvim.org/)">Lvim</a> Neovim Configuration. Aiming to make Neovim usable as a replacement for VSCode
+
+## Status
+
+- [x] _in progress_
+- [ ] _finished_
+- [ ] _no longer continued_
+
+## Table of contents
+
+- [Description](#description)
+- [Status](#status)
+- [Table of contents](#table-of-contents)
+- [General Info](#general-info)
+- [Screenshots](#screenshots)
+- [Technologies](#technologies)
+- [Setup](#setup)
+- [Known Issues](#known-issues)
+
+## General Info
+
+This Project aims to provide a personal portfolio about me and my work, aswell as a blog where
+i write what comes to my mind.
+UNDER CONSTRUCTION
+
+## Screenshots
+
+<![UNDER CONSTRUCTION](./readme/Dashboard.png)
+<![MainView](./readme/MainView.png)
+
+## Technologies
+
+This config configures Plugins with Packer on top of the Lunarvim base-configuration. Added Configs / Plugins are:
+
+- Gui Client: Neovide
+- Hop Quick Search
+- Material Theme
+- MesloLGSDZ Nerd Font
+- Sidebar To The Right
+- Custom Alpha Dashboard
+- Null-Ls config for prettier & eslint
+
+## Setup
+
+Setup neovim & lunarvim. Add this config to
+
+```
+~/config/lvim
+```
+
+Configure Neovide and add it's custom config to your path (see. https://www.lunarvim.org/01-installing.html#tips-for-neovide-users )
+
+## Known Issues
+
+These are Features i want to add to this config in the Future:
+
+```
+- Theming more like Material Legacy theme [ ]
+- Lualine gitblame integration [ ]
+- Custom LazyGit styling  [ ]
+- Neovide [x]
+- uickfix space lq -> mappen to '.' like vscode? [ ]
+- copy paste to cmd-v + p mappen (added to yy + p)? + move line alt+arrow? [ ]
+- styled components & vue TreeSitter highlighting (Maybe styling issue?) [ ]
+- fileexplorer unchangeable root dir ?maybe? [ ]
+- fileexplorer view files (images) + add files from editor?, open images directly not working? [ ]
+- linting ? eslint /prettier not working -> Null-ls support [x]
+- eslint quickfix? [ ]
+- below tabs path to components [ ]
+- autosave ? [ ]
+- line connectors between <div> </div> [ ]
+- rainbow brackets {{}} [ ]
+- collapse code ?already implemented? [ ]
+- autoformat throws eslint errors eg - BuMoleculeARticleBox^
+```
---
+++ config.lua
@@ -0,0 +1,106 @@
+--[[
+lvim is the global options object - based on lvim example config.
+]]
+
+-- general
+vim.o.guifont = "MesloLGSDZ Nerd Font"
+lvim.log.level = "warn"
+lvim.format_on_save = true
+lvim.colorscheme = "material"
+vim.g.material_style = "darker"
+vim.opt.relativenumber = true
+
+-- neovide
+vim.g.neovide_cursor_animation_length = 0.025
+
+-- keymappings [view all the defaults by pressing <leader>Lk]
+lvim.leader = "space"
+-- add your own keymapping
+lvim.keys.normal_mode["<C-s>"] = ":w<cr>" -- save
+lvim.keys.normal_mode["<esc>"] = ":noh<cr>" -- clear ?: searches with esc
+
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "right"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell", "phpdoc" }
+lvim.builtin.treesitter.highlight.enabled = true
+
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    -- required: 'npm i -g prettier'
+    command = "prettier",
+    extra_args = { "--print-with", "100" },
+  },
+}
+
+-- -- set additional linters
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  -- requires: 'npm i -g eslint_d'
+  { command = "eslint_d", filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" } },
+}
+
+-- Additional Plugins
+lvim.plugins = {
+  -- Theme
+  { "marko-cerovac/material.nvim" },
+
+  -- Navigation
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
+
+  -- Git
+  -- TODO: gitblame in lualine
+}
+
+lvim.builtin.alpha.dashboard.section.header.val = {
+  '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀',
+  '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⣠⣦⣄⣀⣴⣦⣄⣈⣻⣿⣿⣿⣿⣿⣿⡇ ',
+  '⠀⠀⠀⠀⠀⠀⣀⡀⣀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀',
+  '⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⡿⠿⠛⠋⠉⠁⠀⠀⠀⠀⠀⠸⢿⣿⣷⣄⣀⠀⠀',
+  '⠀⠀⠀⣶⣶⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠈⠉⠉⠀⠀',
+  '⠀⣀⣠⣿⣿⣿⡿⠟⠁⠀⢀⣄⣀⣠⣦⣀⣠⣄⠀⢹⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀',
+  '⠀⣿⣿⣿⣿⡿⠁⣰⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀',
+  '⠀⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⠟⠛⠉⠉⠉⠉⠙⢻⣿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀',
+  '⠀⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⠁⠀⠀⠹⠛⠁⠀⠀⠀',
+  ' ⣿⣿⣿⣿⣿⣿⣿⣧⣤⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⠄⠀⠀⠀⠀⠀⠀⠀',
+  '⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⡄⠀⠀⢀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀',
+  '⠀⣿⣿⣿⣿⡿⠟⠛⠛⠛⠻⠿⣿⣿⣷⣤⣆⠀⣈⠻⣿⣿⣿⣿⣶⣶⣶⠆⠀⠀',
+  '⠀⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠉⠉⠀⠀⠀',
+  '⠀⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠉⠹⢿⣿⣷⣄⣀⠀⠀⠀⠀',
+  '⠀⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀'
+}
