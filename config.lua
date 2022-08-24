588c0e348c796854f5fa522d070d7883cfe2f4c4
Jonas Leonhard <jonasleonhardfl@gmail.com>
2022-08-24 23:23:58 +0200

    fix(config.lua): added highlighting of folded lines / columns

--- config.lua
+++ config.lua
@@ -102,6 +102,18 @@ if (ufold_ok) then
   vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
   vim.o.foldenable = true
   vim.opt.foldlevelstart = 99
+
+  vim.api.nvim_create_autocmd("BufEnter", {
+    command = "highlight FoldColumn guibg=#161616",
+  })
+  vim.api.nvim_create_autocmd("BufEnter", {
+    command = "highlight Folded guibg=#161616",
+  })
+  -- reset cursorLine to theme background, as folded / foldcolumn also alters cursorLine background
+  vim.api.nvim_create_autocmd("BufEnter", {
+    command = "highlight CursorLine guibg=#212121",
+  })
+
   ufold.setup({
     provider_selector = function(bufnr, filetype, buftype)
       return { 'treesitter', 'indent' }
