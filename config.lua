3f023253c930b4f019d6ab38498ef59ee1ece169
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2022-09-08 12:31:02 +0200

    fix(config.lua): smooth scroll issues temp fix

--- config.lua
+++ config.lua
@@ -14,6 +14,7 @@ vim.o.syntax = "on"
 
 -- neovide
 vim.g.neovide_cursor_animation_length = 0.025
+vim.g.neovide_scroll_animation_length = 0 -- HACK: disabled because fold cause neovide smooth scroll issues for now
 
 -- autocommands
 vim.api.nvim_create_autocmd("BufEnter", {
