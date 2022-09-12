0f3faad352c5f7f346abc6ddd5bd60dc1e3e33c3
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2022-09-12 12:55:02 +0200

    feat(null-ls): added twig prettier formatting

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -1,15 +1,23 @@
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  {
-    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-    command = "prettier",
-    extra_args = { "--print-with", "100" },
-  },
-}
+local null_ls_ok, null_ls = pcall(require, "null-ls")
 
--- -- set additional linters
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "eslint" },
-}
+if (null_ls_ok) then
+  local formatters = require "lvim.lsp.null-ls.formatters"
+  local prettierFiletypes = null_ls.builtins.formatting.prettier.filetypes
+  table.insert(prettierFiletypes, 'htmldjango.twig');
+
+  formatters.setup {
+    {
+      -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+      command = "prettier",
+      filetypes = prettierFiletypes,
+      extra_args = { "--print-with", "100" },
+    },
+  }
+
+  -- -- set additional linters
+  local linters = require "lvim.lsp.null-ls.linters"
+  linters.setup {
+    { command = "eslint" },
+  }
+end
