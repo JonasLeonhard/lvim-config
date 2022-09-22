5a31dced2ea34127d1c0b5cf1e44bcd741d5ab9f
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2022-09-22 17:39:34 +0200

    fix(terminal): changed terminal open keybinding

--- lua/user/terminal.lua
+++ lua/user/terminal.lua
@@ -1,6 +1,6 @@
 lvim.builtin.terminal.active = true -- setup in terminal.lua
-local status_ok, toggleterm = pcall(require, "toggleterm")
-if not status_ok then
+local toggleterm_ok, toggleterm = pcall(require, "toggleterm")
+if not toggleterm_ok then
   return
 end
 
@@ -8,4 +8,5 @@ end
 --   width = 100,
 --   height = 100
 -- } -- TODO: get full width / height
-lvim.builtin.terminal.open_mapping = "<C-t>"
+lvim.builtin.terminal.open_mapping = "<C-/>"
+lvim.builtin.terminal.open_mapping = "<C-7>"
