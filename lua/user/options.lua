647600bda808697f27bff17887a067d4bf1f0ee1
Jonas Leonhard <jonasleonhardfl@gmail.com>
2022-09-22 18:56:23 +0200

    fix(options): moved navic setup to navic.lua

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -7,20 +7,3 @@ vim.opt.relativenumber = true
 vim.g.syntax_on = true
 vim.o.syntax = "on"
 lvim.builtin.autopairs.active = false
-
-lvim.builtin.breadcrumbs.active = true
-local navic = require("nvim-navic")
-lvim.lsp.on_attach_callback = function(client, bufnr)
-  if client.server_capabilities.documentSymbolProvider then
-    navic.attach(client, bufnr)
-  end
-end
-local function getnavic()
-  if navic.is_available() then
-    return navic.get_location()
-  else
-    return
-  end
-end
-
-lvim.builtin.lualine.sections.lualine_c = { { getnavic } }
