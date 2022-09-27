7b122f03745ec9e12c670e4b547f6ce63d1034db
Jonas Leonhard <jonasleonhardfl@gmail.com>
2022-09-27 13:31:46 +0200

    fix(hop): removed duplicate keymaps

--- lua/user/hop.lua
+++ lua/user/hop.lua
@@ -10,19 +10,6 @@ keymap("", "L", ":HopWordCurrentLine<cr>", { silent = true })
 vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
 vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
 
-keymap("o", "f",
-  ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>"
-  , opts)
-keymap("o", "F",
-  ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<CR>"
-  , opts)
-keymap("o", "t",
-  ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })<CR>"
-  , opts)
-keymap("o", "T",
-  ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })<CR>"
-  , opts)
-
 keymap("n", "f",
   ":lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<CR>"
   , opts)
