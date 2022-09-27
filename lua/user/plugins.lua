4e29d35130686fdbfe9cb11be634c85141f4d0cc
Jonas Leonhard <jonasleonhardfl@gmail.com>
2022-09-27 13:24:50 +0200

    fix(hop): added hop keybindings

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -6,11 +6,6 @@ lvim.plugins = {
   {
     "phaazon/hop.nvim",
     event = "BufRead",
-    config = function()
-      require("hop").setup()
-      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
-    end,
   },
   { "ThePrimeagen/harpoon", requires = "nvim-lua/plenary.nvim" },
   -- In-Editor
