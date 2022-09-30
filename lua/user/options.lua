17ef2c9fac891e7236a16c44849ee0814f925ee9
Jonas Leonhard <jonasleonhardfl@gmail.com>
2022-10-01 00:45:41 +0200

    fix(options): added global options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,10 +1,18 @@
+vim.g.catppuccin_flavour = "mocha"
+vim.g.syntax_on = true
+
 vim.o.guifont = "MesloLGSDZ Nerd Font:h15" --:h<FontSize>
-lvim.log.level = "warn"
+vim.o.syntax = "on"
+
+vim.opt.relativenumber = true
+vim.opt.conceallevel = 0 -- makes hidden whitespace visiable
+vim.opt.undofile = true -- persisten undo
+vim.opt.smartindent = true
+vim.opt.smartcase = true
+
 lvim.format_on_save = true
 lvim.colorscheme = "catppuccin"
-vim.g.catppuccin_flavour = "mocha"
-vim.opt.relativenumber = true
-vim.g.syntax_on = true
-vim.o.syntax = "on"
+lvim.log.level = "warn"
 lvim.builtin.autopairs.active = false
 lvim.builtin.cmp.experimental.ghost_text = true
+lvim.builtin.treesitter.highlight.enabled = true
