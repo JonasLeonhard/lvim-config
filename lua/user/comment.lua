0d21012decf64d96b5036859c1091e45872d1331
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2022-09-30 15:04:39 +0200

    fix(comment): added todo

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -3,5 +3,7 @@ local ok, ft = pcall(require, "Comment.ft")
 if not ok then
   return
 end
+
+-- TODO: this does not override comments
 ft.set('lua', { '--[[%s]]', '--[[%s]]' });
 ft.set('html.twig', { '{#%s#}', '{#%s#}' });
