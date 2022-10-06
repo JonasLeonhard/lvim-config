34bdb226982b49c5a9fe1483a7da2d382dd0d460
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2022-10-06 15:57:46 +0200

    fix(lsp-signature): added signature to lualine

--- lua/user/lsp-signature.lua
+++ lua/user/lsp-signature.lua
@@ -6,7 +6,18 @@ end
 
 lsp_signature.setup({
   floating_window = false,
-  hint_enable = true,
+  hint_enable = false,
   always_trigger = false,
   hint_prefix = ""
 })
+
+local current_signature = function(width)
+  if not pcall(require, 'lsp_signature') then return end
+  local sig = require("lsp_signature").status_line(width)
+  if sig.label == "" then
+    return ""
+  end
+  return sig.label .. "  " .. sig.hint
+end
+
+lvim.builtin.lualine.sections.lualine_c = { { function() return current_signature() end } };
