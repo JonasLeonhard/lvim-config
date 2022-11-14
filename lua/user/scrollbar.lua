2546e675a82c468a4b236a2097a38367d7425b4b
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2022-11-14 18:06:56 +0100

    fix(scrollbar): added gitsigns integration

--- lua/user/scrollbar.lua
+++ lua/user/scrollbar.lua
@@ -1,4 +1,9 @@
 local scrollbar_ok, scrollbar = pcall(require, "scrollbar")
+local scrollbarGitsignIntegration_ok, scrollbarGitsignIntegration = pcall(require, "scrollbar.handlers.gitsigns")
+
 if (scrollbar_ok) then
+  if (scrollbarGitsignIntegration_ok) then
+    scrollbarGitsignIntegration.setup()
+  end
   scrollbar.setup()
 end
