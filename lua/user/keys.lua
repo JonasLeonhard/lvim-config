7fe545b301e8ccb189ee95c09aa287246d626d66
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-01-12 18:32:36 +0100

    fix(keys): added delete all marks keybind

--- lua/user/keys.lua
+++ lua/user/keys.lua
@@ -78,7 +78,8 @@ lvim.builtin.which_key.mappings["m"] = {
     b = { "<cmd>MarksListBuf<cr>", "List Buffer" },
     g = { "<cmd>MarksListGlobal<cr>", "List Global" },
     h = { "<cmd>:lua print('[mx] = set Mark <x>') print('[m,] = Set the next alphabetical lowercase mark') print('[dmx] = delete mark <x>') print('[dm-] = delete all marks on the current line') print('[dm<space>] = delete all marks in buffer') print('[m]] = move to next mark') print('[m[] = move to prev mark') print('[m:] = preview mark') print('[m0-9] = bookmark from bookmarkgroup') print('[dm0-9] = delete bookmark from bookmarkgroup') print('[m{] = move to next bookmark') print('[m}] = move to prev bookmark') print('[dm=] = delete bookmark under cursor')<cr>",
-      "Keybind help" }
+      "Keybind help" },
+    d = { "<cmd>:delmarks A-Z0-9<cr>", "Delete all marks globally" }
   }
 }
 
