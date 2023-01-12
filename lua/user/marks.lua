bf84f30d83dbfe47f46ce32b7f1864baa7542b3b
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-01-12 18:39:55 +0100

    fix(marks): fixed marks reloading after they where deleted

--- lua/user/marks.lua
+++ lua/user/marks.lua
@@ -7,4 +7,6 @@ end
 marks.setup({
   -- while lower values may cause performance penalties. default 150.
   refresh_interval = 300,
+  -- whether the shada file is updated after modifying marks. default false. Without this cleared marks are reloaded after saving.
+  force_write_shada = true,
 })
