c149da67e46f3fbd30116b99d4313fd656befc70
Jonas Leonhard <jonasleonhardfl@gmail.com>
2023-01-13 16:09:21 +0100

    fix(marks.lua): added sign priority above gitsigns

--- lua/user/marks.lua
+++ lua/user/marks.lua
@@ -9,4 +9,5 @@ marks.setup({
   refresh_interval = 300,
   -- whether the shada file is updated after modifying marks. default false. Without this cleared marks are reloaded after saving.
   force_write_shada = true,
+  sign_priority = 100
 })
