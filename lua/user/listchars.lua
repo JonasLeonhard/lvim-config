1ef12ae6d9997adafc6ec9b5f5d2d11b387ca66b
jonas.leonhard@fork.de <jonas.leonhard@fork.de>
2023-02-20 10:40:56 +0100

    fix(listchars): fixed highlighting in alpha dashboard

--- lua/user/listchars.lua
+++ lua/user/listchars.lua
@@ -8,14 +8,40 @@ local themeColors = require("catppuccin.palettes").get_palette "mocha";
 vim.opt.list = true
 vim.opt.listchars = "tab:  ,trail:·,nbsp:·"
 
+
+print(vim.fn.expand('%'));
+local highlightListchars = function()
+  local filename = vim.fn.expand('%');
+
+  -- skip highlighting for non-files (eg- alpha dashboard.)
+  if (filename == '') then
+    return
+  end
+
+  -- https://vim.fandom.com/wiki/Highlight_unwanted_spaces#Highlighting_with_the_match_command
+  vim.cmd [[ match NBSP /\u00a0/ ]]
+  vim.cmd [[ match TrailingSpaceChar /\s\+$/ ]]
+  vim.api.nvim_set_hl(0, "NBSP",
+    { fg = "White", bg = themeColors.red })
+  vim.api.nvim_set_hl(0, "TrailingSpaceChar",
+    { fg = "White", bg = themeColors.red })
+end
+
+
+local deHighlightListchars = function()
+  -- https://vim.fandom.com/wiki/Highlight_unwanted_spaces#Highlighting_with_the_match_command
+  vim.api.nvim_set_hl(0, "NBSP", {})
+  vim.api.nvim_set_hl(0, "TrailingSpaceChar", {})
+end
+
 vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-      -- https://vim.fandom.com/wiki/Highlight_unwanted_spaces#Highlighting_with_the_match_command
-      vim.cmd [[ match NBSP /\u00a0/ ]]
-      vim.cmd [[ match TrailingSpaceChar /\s\+$/ ]]
-      vim.api.nvim_set_hl(0, "NBSP",
-          { fg = "White", bg = themeColors.red })
-      vim.api.nvim_set_hl(0, "TrailingSpaceChar",
-          { fg = "White", bg = themeColors.red })
-    end
+  callback = highlightListchars
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  callback = deHighlightListchars
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = highlightListchars
 })
